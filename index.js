412f4ff6fb97d75ffcf06b621405fec9b515cdd6
Andrew Marabante <andrewmarabante@gmail.com>
2023-08-18 14:11:02 -0700

    added a tree class and buildtree function that takes in an array and creates a balanced tree, returning the root

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ class Node
 {
     constructor(data = null, left = null, right = null)
     {
+        console.log('wokring')
         this.data = data;
         this.left = left;
         this.right = right
@@ -12,13 +13,25 @@ class tree
 {
     constructor(array = null)
     {
-        this.root = array;
+        if(array!= null)
+        {
+            this.root = buildTree(array);
+        }
+        else{this.root = array}
     }
 
-    buildTree(array)
-    {
+}
 
-    }
+function buildTree(array, start = 0, end = array.length-1)
+{
+    array = mergeSort(array);
+    if (start>end){return null};
+    let mid = Math.floor((start+end)/2);
+    let root = new Node(array[mid]);
+    console.log(root)
+    root.left = (buildTree(array,start,mid-1));
+    root.right = (buildTree(array,mid+1,end));
+    return root;
 }
 
 const mergeSort = (array) =>
@@ -71,5 +84,19 @@ function merge(left, right)
     return merge;
 }
 
-const newTree = new tree(23)
-console.log(mergeSort([21,56,34,98, 1]))
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+const newTree = new tree([23,12,34,15,54,67,8,2,1,5,89,45,6,31])
+console.log(newTree)
+console.log(prettyPrint(newTree.root))
